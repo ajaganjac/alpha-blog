3e015b5b3ddd509bbbeb46c982554e9afbe4555d
Amer <amer.jaganjac@yahoo.com>
2018-01-10 15:11:19 -0500

    Ik heb artikelen tabel, model en validatie aangemaakt

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ActiveRecord::Base
   
+  validates :title, presence: true, lenght { minimum: 3, maximum: 50 }
+  validates :description, presence: true, lenght { minimum: 10, maximum: 500 }
+  
+  
+  
 end
