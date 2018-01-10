e1e77ce0a5762ff5675c01a005c8870dd8178ed0
Amer <amer.jaganjac@yahoo.com>
2018-01-10 15:51:16 -0500

    Ik heb controllers voor toevoegen van article naar database aangemaakt

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   
-  validates :title, presence: true, lenght { minimum: 3, maximum: 50 }
-  validates :description, presence: true, lenght { minimum: 10, maximum: 500 }
+  validates :title, presence: true
+  validates :description, presence: true
   
   
   
