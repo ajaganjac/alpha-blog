b711d98e5940badc45bcd03e67cec46353a8242e
Amer <amer.jaganjac@yahoo.com>
2018-01-11 14:41:03 -0500

    authenticatie bijgewerkt en ook login en logout routes en sessions aangemaakt

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'pages#home'
+  
+  get 'login', to: 'sessions#new'
+
+  post 'login', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
 
   get 'about', to: 'pages#about'
   
@@ -12,6 +18,7 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   
   
+
   
   
 end
